e98616806d64d29780dfa8d86a65293d74866960
banashankari <banashankaribanashankari4@gmail.com>
2026-06-17 13:13:41 +0530

    Clear repo - keep only README and gitignore

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
- 
-const BASE = 'https://smart-email-system.onrender.com/api';
- 
-export const analyzeEmail = (emailData, language = 'English') =>
-  axios.post(`${BASE}/email/analyze`, { ...emailData, language }).then(r => r.data);
- 
-export const regenerateReply = (emailData, language = 'English') =>
-  axios.post(`${BASE}/email/regenerate-reply`, { ...emailData, language }).then(r => r.data);
